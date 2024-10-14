669f4bd8ce33cf216a6f07b6325cfbdc5becb2f3
Amit Banjare <amitbanjare79@gmail.com>
2024-10-14 11:29:04 +0530

    ioefwojewf

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyD0flMB5yk3HZYjXMQIGL25IUn92VZ4gH8',
-  authDomain: 'meet-clone-d6071.firebaseapp.com',
-  projectId: 'meet-clone-d6071',
-  storageBucket: 'meet-clone-d6071.appspot.com',
-  messagingSenderId: '449972301617',
-  appId: '1:449972301617:web:e46fd0f0c5b10109a40a10',
-  measurementId: 'G-GRD7KVE0YH',
+  apiKey: "AIzaSyB1LmQD9L_CRwTp4WzWhHs041-dxavzjg0",
+  authDomain: "vertex-meet.firebaseapp.com",
+  projectId: "vertex-meet",
+  storageBucket: "vertex-meet.appspot.com",
+  messagingSenderId: "429369736656",
+  appId: "1:429369736656:web:330b3e1236f1ebe6568dd5",
+  measurementId: "G-L28THB90EF",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
@@ -21,3 +21,25 @@ const providerGoogle = new firebase.auth.GoogleAuthProvider();
 const providerGithub = new firebase.auth.GithubAuthProvider();
 
 export { db, auth, providerGoogle, providerGithub };
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB1LmQD9L_CRwTp4WzWhHs041-dxavzjg0",
+//   authDomain: "vertex-meet.firebaseapp.com",
+//   projectId: "vertex-meet",
+//   storageBucket: "vertex-meet.appspot.com",
+//   messagingSenderId: "429369736656",
+//   appId: "1:429369736656:web:330b3e1236f1ebe6568dd5",
+//   measurementId: "G-L28THB90EF"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
